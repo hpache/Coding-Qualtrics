c72e385fc214aba28267f9e06c669227b5ef1d66
AidanSweeny <apswee22@colby.edu>
2022-03-17 12:09:55 -0400

    Initial styling for run button and errors:

--- Codetrics/src/script.js
+++ Codetrics/src/script.js
@@ -1,9 +1,22 @@
 
 window.addEventListener('load', function () {
-    var myCodeMirror = CodeMirror(document.body, {
+    container = document.getElementById("mirror-container");
+    var myCodeMirror = CodeMirror(container, {
         value: "function myScript(){return 100;}\n",
         mode:  "javascript",
         theme: "oceanic-next",
         lineNumbers: true
     });
+    button = document.createElement("button");
+    errors = document.createElement("p");
+    div = document.createElement("div");
+    div.setAttribute("class", "error-container");
+    errors.setAttribute("class", "errors-display");
+    button.setAttribute("class", "run-button");
+    button.innerText = "Run";
+    errors.innerText = "Error Messages";
+    div.appendChild(errors);
+    div.appendChild(button);
+    container.appendChild(div);
+    console.log(myCodeMirror);
 });
