2743564d6b2970c9e6d31ad08fbcd451fb4ea31b
hpache <shenryp78@gmail.com>
2022-04-12 11:18:49 -0400

    Trying to get angular to work

--- src/surveys/NASA-TLX.php
+++ src/surveys/NASA-TLX.php
@@ -5,10 +5,24 @@
     <meta charset="utf-8">
     <title>Codetrics</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/x-icon" href="../favicon.ico">
+    <script src="../node_modules/angular/angular.js"></script>
     </head>
 
     <form>
+        <mat-slider aria-label="unit(s)"></mat-slider>
+        <br>
+        <input type="range" min="0" max="100" id="mental" ngmodel="mat-slider" >
+        <br>
+        <input type="range" min="0" max="100" id="physical">
+        <br>
+        <input type="range" min="0" max="100" id="time-pressure">
+        <br>
+        <input type="range" min="0" max="100" id="success">
+        <br>
+        <input type="range" min="0" max="100" id="complexity">
+        <br>
+        <input type="range" min="0" max="100" id="stress">
     </form>
 
 </html>
