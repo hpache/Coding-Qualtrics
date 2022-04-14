2466d76d6fbcc8a7ea8756084d3af4ec1ed86b40
hpache <shenryp78@gmail.com>
2022-04-13 21:41:23 -0400

    Added mongodb library and extension for php

--- run-apache2.sh
+++ run-apache2.sh
@@ -3,4 +3,6 @@
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 apache2ctl -D FOREGROUND
 htpasswd -b -c /etc/apache2/.htpasswd admin ${{secrets.ADMIN}}
+a2enmod headers
 apache2ctl restart
+
