1cdb91c22581d580a03904aef44d11e5d18a6215
hpache <shenryp78@gmail.com>
2022-04-14 01:43:14 -0400

    Got routing ready for demo

---
+++ src/experiment/bad_identifiers/Die.java
@@ -0,0 +1,102 @@
+/*
+The following program simulates a die rolling.  
+The program contains a bug, and you are expected to debug this program and fix the bug.  
+
+Here’s a representation of the intended program behavior:
+_  _  _
+Every tenth roll:
+[63, 61, 50, 55, 32, 9, 16, 10, 70, 31]
+_  _  _
+
+Keep in mind that this program uses random functionality, and while you should
+expect to see ten numbers as shown above, the numbers will not necessarily
+exactly match those in the example representation. 
+
+
+The program should do the following:
+1-	Roll the die 100 times.
+2-	Add every the number on the side of the die that faces up after every tenth roll to a list.
+3-	Print that list.
+
+
+The bug(s) could be anywhere in the code. 
+Try to run the program a few times with different input and check if the output makes sense or not.
+
+You cannot ask the experimenter for questions or skip to the next program.  
+
+If you cannot fix the program, you can procced to the next problem without coming back to this program.
+*/
+
+import java.util.ArrayList;							
+import java.util.Random;							
+import java.util.*; 								
+
+public class Die 
+{
+    private int sideUp;	
+    private int numSides;
+    							
+    private ArrayList array = new ArrayList();
+    
+    public Die(int numSides)
+    {
+    	this.sideUp = numSides;
+    	this.numSides = numSides;
+    }
+    
+    public void roll() 								
+    {
+        this.sideUp = (int) (Math.random() * this.numSides + 1);
+    }
+    
+    public int getSideUp() 
+    {
+        return this.sideUp;
+    }
+    
+    public String toString()
+    {
+    	return "You rolled a " + this.sideUp;
+    }
+    
+    public boolean equals(Object o)
+    {
+    	if (! (o instanceof Die))
+    	{
+    		return false;
+    	}
+    	
+    	Die otherDie = (Die) o;
+    	if (this.sideUp == otherDie.sideUp)
+    	{
+    		return true;
+    	}
+    	
+    	else
+    	{
+    		return false;
+    	}
+    }
+    
+    
+    public static void main(String[] args) 			
+    {
+		Die myDie = new Die(100);
+		List<Integer> results = new ArrayList<Integer>(); 
+		
+		for(int count=0; count<100; count++)
+		{
+			myDie.roll();
+            int number = myDie.getSideUp();
+			
+			if ((number / 10) == 0) 
+			{
+				results.add(myDie.getSideUp());
+			}
+		}
+		
+		System.out.println("Every tenth roll:");
+		System.out.println(results);
+		
+    }
+}
---
+++ src/scripts/php/experiment.php
@@ -0,0 +1,59 @@
+<?php
+
+
+$filepath = $_SERVER["DOCUMENT_ROOT"] . "/plans/experimentlayout.csv";
+$csvData = file_get_contents($filepath);
+$lines = explode(PHP_EOL, $csvData);
+$array = array();
+foreach ($lines as $line) {
+    $array[] = str_getcsv($line);
+}
+
+$row = 0;
+$currentProgram = "";
+# Iterate over each array
+foreach ($array as $element){
+
+    $program = $element[0];
+    $picked = $element[1];
+
+    # Find first with second element with 0
+    if ($picked == 0){
+
+        # Switch value to 1 then break loop
+        $array[$row][1] = "1";
+        # Assign to current program
+        $currentProgram = $program;
+        break;
+    }
+
+    # Add one to count
+    $row++;
+
+}
+
+# Pick random integer
+$rand_int = rand(2, 3);
+# Get corresponding identifier
+$identifier = $array[$row][$rand_int];
+
+// Open a file in write mode ('w')
+$fp = fopen($filepath, 'w');
+
+// Loop through file pointer and a line
+foreach ($array as $fields) {
+    fputcsv($fp, $fields);
+}
+
+# Create program path
+$porgrampath = $_SERVER["DOCUMENT_ROOT"] . "/experiment/" . $identifier . "/" . $program;
+
+# read program
+$myfile = fopen($porgrampath, "r") or die("Unable to open file!");
+
+
+echo "$row" . fread($myfile,filesize($porgrampath));
+
+
+
+?>
